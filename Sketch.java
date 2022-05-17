bb3b5f3b44aaf8be58ace051bdebb394606862ab
Marcus Lohbihler <75390882+MarcusLohbihler@users.noreply.github.com>
2022-05-17 01:33:27 +0000

    added comments

--- Sketch.java
+++ Sketch.java
@@ -26,6 +26,7 @@ public class Sketch extends PApplet{
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+    //Initializing locations of the snowballs in the array
     for (int i = 0; i < circleY.length; i++) {
       circleY[i] = random(height);
       ballHideStatus[i] = true;
@@ -34,7 +35,7 @@ public class Sketch extends PApplet{
 
   public void draw() {
     background(50);
-  
+    //for loop that controls snowball movement, collision detection and lives
     for (int i = 0; i < circleY.length; i++) {
       circleX = width * i / circleY.length;
       if(ballHideStatus[i] == true){
@@ -42,9 +43,9 @@ public class Sketch extends PApplet{
         fill(255);
         ellipse(circleX, circleY[i], 25, 25);
       }
-
+      //initial movement speed of the circles
       circleY[i] += 2;
-      
+      //if up arrow is pressed, decreases snowball speed, if down arrow is pressed, increases snowball speed
       if(keyPressed){
         if(keyCode == UP){
           circleY[i] --;
@@ -52,13 +53,14 @@ public class Sketch extends PApplet{
           circleY[i] += 4;
         }
       }
-      
+
+      //if snowballs go offscreen, bring them back to the top, invisible circles reappear
       if (circleY[i] > height) {
         ballHideStatus[i] = true;
         circleY[i] = 0;
       }
   
-      
+      //if the player hits a snowball, the snowball disappears and the life count decreases by one, if the mouse is placed on top of a snowball, the snowball disappears
       if (ballHideStatus[i]) {
         if(playerX >= circleX - 12.5 && playerX <= circleX + 12.5 && playerY >= circleY[i] - 12.5 && playerY <= circleY[i] + 12.5){
           ballHideStatus[i] = false;
@@ -67,7 +69,8 @@ public class Sketch extends PApplet{
           ballHideStatus[i] = false;
         }
       }
-  
+
+      //shows how many lives are remaining, creates game over screen once the life count reaches zero or goes negative
       if(lifeCount == 3){
         fill(255, 0, 0);
         rect(310, 10, 20, 20);
@@ -89,10 +92,11 @@ public class Sketch extends PApplet{
         text(endGame, 50, 200);
       }
     }
-    
+    //creates player
     fill(0,255,255);
     ellipse(playerX, playerY, 30, 30);
-    
+
+    //player movement controls
     if(keyPressed){
       if(key == 'a'){
         playerX -= 3;
@@ -104,6 +108,7 @@ public class Sketch extends PApplet{
         playerY += 3;
       }
     }
+    //if the player tries to go offscreen, bring them back to the starting point
     if(playerX > width || playerX < 0 || playerY > height || playerY < 0){
       playerX = 200;
       playerY = 300;
