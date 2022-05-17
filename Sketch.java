a6868e89ff75098746f6eeb8a78aa92b4c42ace9
Marcus Lohbihler <75390882+MarcusLohbihler@users.noreply.github.com>
2022-05-17 00:58:16 +0000

    completed assignment

--- Sketch.java
+++ Sketch.java
@@ -1,7 +1,16 @@
 import processing.core.PApplet;
 
-public class Sketch extends PApplet {
-	
+public class Sketch extends PApplet{ 
+  int ballCount = 25;
+  float playerX = 200;
+  float playerY = 300;
+  double playerRad = 15;
+  float circleX;  
+  float[] circleY = new float[ballCount];
+  double circleRad = 12.5;
+  double distance = 0;
+  boolean[] ballHideStatus = new boolean[ballCount];
+  int lifeCount = 3;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -10,27 +19,94 @@ public class Sketch extends PApplet {
 	// put your size call here
     size(400, 400);
   }
+	
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+      ballHideStatus[i] = true;
+    }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    background(50);
   
-  // define other methods down here.
+    for (int i = 0; i < circleY.length; i++) {
+      circleX = width * i / circleY.length;
+      if(ballHideStatus[i] == true){
+        stroke(0);
+        fill(255);
+        ellipse(circleX, circleY[i], 25, 25);
+      }
+
+      circleY[i] += 2;
+      
+      if(keyPressed){
+        if(keyCode == UP){
+          circleY[i] --;
+        }else if(keyCode == DOWN){
+          circleY[i] += 4;
+        }
+      }
+      
+      if (circleY[i] > height) {
+        ballHideStatus[i] = true;
+        circleY[i] = 0;
+      }
+  
+      
+      if (ballHideStatus[i]) {
+        if(playerX >= circleX - 12.5 && playerX <= circleX + 12.5 && playerY >= circleY[i] - 12.5 && playerY <= circleY[i] + 12.5){
+          ballHideStatus[i] = false;
+          lifeCount --;
+        }else if(mouseX >= circleX - 12.5 && mouseX <= circleX + 12.5 && mouseY >= circleY[i] - 12.5 && mouseY <= circleY[i] + 12.5){
+          ballHideStatus[i] = false;
+        }
+      }
+  
+      if(lifeCount == 3){
+        fill(255, 0, 0);
+        rect(310, 10, 20, 20);
+        rect(340, 10, 20, 20);
+        rect(370, 10, 20, 20);
+      }else if(lifeCount == 2){
+        fill(255, 0, 0);
+        rect(340, 10, 20, 20);
+        rect(370, 10, 20, 20);
+      }else if(lifeCount == 1){
+        fill(255, 0, 0);
+        rect(370, 10, 20, 20);
+      }else if(lifeCount <= 0){
+        background(255);
+        circleY[i] += 0;
+        fill(0);
+        String endGame = ("Game Over");
+        textSize(50);
+        text(endGame, 50, 200);
+      }
+    }
+    
+    fill(0,255,255);
+    ellipse(playerX, playerY, 30, 30);
+    
+    if(keyPressed){
+      if(key == 'a'){
+        playerX -= 3;
+      }else if(key == 'd'){
+        playerX += 3;
+      }else if(key == 'w'){
+        playerY -= 3;
+      }else if(key == 's'){
+        playerY += 3;
+      }
+    }
+    if(playerX > width || playerX < 0 || playerY > height || playerY < 0){
+      playerX = 200;
+      playerY = 300;
+    }  
+  }
 }
